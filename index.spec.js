31d6e7ee26e91f3c211ad0e8b0998e29083b2f75
3imed-jaberi <imed_jebari@hotmail.fr>
2020-06-14 04:29:08 +0200

    use koa@2.x.x --test 🧪..

---
+++ index.spec.js
@@ -0,0 +1,40 @@
+const assert = require('assert');
+const stream = require('stream');
+const Koa = require('koa');
+const request = require('supertest');
+const accesslog = require('.');
+
+const app = new Koa();
+const log = new stream.PassThrough();
+
+app.use(accesslog(log));
+
+const pattern = /((?:\d{1,3}\.){3}\d{1,3}).*"(\w{3}) \//;
+
+describe('lib accesslog', function () {
+  let agent;
+  let server;
+
+  before(function (done) {
+    server = app.listen(done);
+    agent = request.agent(server);
+  });
+
+  after(function () {
+    server.close();
+  });
+
+  it('should output an access log', function (done) {
+    agent.get('/').end(function (err) {
+      if (err) {
+        done(err);
+        return;
+      }
+
+      const match = pattern.exec(log.read());
+      assert.equal(match[1], '127.0.0.1');
+      assert.equal(match[2], 'GET');
+      done();
+    });
+  });
+});
